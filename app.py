0215330005a431017559978432825b68b1d1b981
alkaren.ichsan <alkaren.ichsan@merdekacoppergold.com>
2024-08-02 14:26:36 +0700

    New Repo

--- app.py
+++ app.py
@@ -1,18 +1,123 @@
-# 3rd parth dependencies
-from flask import Flask
+import os
 from flask_cors import CORS
 
 # project dependencies
 from deepface import DeepFace
 from deepface.api.src.modules.core.routes import blueprint
 from deepface.commons import logger as log
+from deepface.api.src.modules.core import service
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+   
+@app.route("/represent", methods=["POST"])
+def represent():
+    input_args = request.get_json()
+
+    if input_args is None:
+        return {"message": "empty input set passed"}
+
+    img_path = input_args.get("img") or input_args.get("img_path")
+    if img_path is None:
+        return {"message": "you must pass img_path input"}
+
+    obj = service.represent(
+        img_path=img_path,
+        model_name=input_args.get("model_name", "VGG-Face"),
+        detector_backend=input_args.get("detector_backend", "opencv"),
+        enforce_detection=input_args.get("enforce_detection", True),
+        align=input_args.get("align", True),
+        anti_spoofing=input_args.get("anti_spoofing", False),
+        max_faces=input_args.get("max_faces"),
+    )
+
+    logger.debug(obj)
+
+    return obj
+
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    input_args = request.get_json()
+
+    if input_args is None:
+        return {"message": "empty input set passed"}
+
+    img1_path = input_args.get("img1") or input_args.get("img1_path")
+    img2_path = input_args.get("img2") or input_args.get("img2_path")
+
+    if img1_path is None:
+        return {"message": "you must pass img1_path input"}
+
+    if img2_path is None:
+        return {"message": "you must pass img2_path input"}
+
+    verification = service.verify(
+        img1_path=img1_path,
+        img2_path=img2_path,
+        model_name=input_args.get("model_name", "VGG-Face"),
+        detector_backend=input_args.get("detector_backend", "opencv"),
+        distance_metric=input_args.get("distance_metric", "cosine"),
+        align=input_args.get("align", True),
+        enforce_detection=input_args.get("enforce_detection", True),
+        anti_spoofing=input_args.get("anti_spoofing", False),
+    )
+
+    logger.debug(verification)
+
+    return verification
+
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    input_args = request.get_json()
+
+    if input_args is None:
+        return {"message": "empty input set passed"}
+
+    img_path = input_args.get("img") or input_args.get("img_path")
+    if img_path is None:
+        return {"message": "you must pass img_path input"}
+
+    demographies = service.analyze(
+        img_path=img_path,
+        actions=input_args.get("actions", ["age", "gender", "emotion", "race"]),
+        detector_backend=input_args.get("detector_backend", "opencv"),
+        enforce_detection=input_args.get("enforce_detection", True),
+        align=input_args.get("align", True),
+        anti_spoofing=input_args.get("anti_spoofing", False),
+    )
+
+    logger.debug(demographies)
+
+    return demographies
+
+   
 logger = log.get_singletonish_logger()
 
-
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-    app.register_blueprint(blueprint)
-    logger.info(f"Welcome to DeepFace API v{DeepFace.__version__}!")
-    return app
+if __name__ == '__main__':
+   CORS(app)
+   app.run()
